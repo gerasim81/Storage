a48e463feecb596657b25af9a1756226be6c3611
Spoloborota <spoloborota@yandex.ru>
2016-06-03 23:43:00 +0300

    First commit

---
+++ src/main/java/com/spoloborota/teaching/storage/commands/Commands.java
@@ -0,0 +1,17 @@
+package com.spoloborota.teaching.storage.commands;
+
+public interface Commands {
+	public final static String DISPLAY = "display";
+	public final static String CREATE = "create";
+	public final static String USE = "use";
+	public final static String SHUTDOWN = "shutdown";
+	
+	public final static String MAP = "map";
+	public final static String LIST = "list";
+	public final static String SET = "set";
+	public final static String QUEUE = "queue";
+	
+	public final static String ADD = "add";
+	public final static String SELECT_ALL = "select all";
+	
+}
