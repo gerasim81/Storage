c4651df6b8f5434b5e21cc3419d2db7a0a188b7b
gerasim81 <gerasim81@rambler.ru>
2016-11-22 08:23:07 +0300

    Remove Show, add List commit.

--- src/main/java/com/spoloborota/teaching/storage/processor/type/List.java
+++ src/main/java/com/spoloborota/teaching/storage/processor/type/List.java
@@ -2,12 +2,12 @@ package com.spoloborota.teaching.storage.processor.type;
 
 import com.spoloborota.teaching.storage.model.RAM;
 
-public class Show {
+public class List {
 	
 	public static String process(RAM ram, String[] commandWords) {
-		boolean isShowed = ram.list(new String[]{commandWords[0]});
-		if (isShowed) {
-			return "Data showed";
+		String isListed = ram.list(new String[]{commandWords[0]});
+		if (isListed != null) {
+			return isListed;
 		} else {
 			return "There is no selected storage";
 		}
